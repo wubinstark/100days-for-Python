b8561756d263f051a499d34adac746531a82a3ec
Logani <2322101957@qq.com>
2020-08-07 22:25:02 +0800

    0807

--- aqi_v8.0.py
+++ aqi_v8.0.py
@@ -11,7 +11,21 @@ def main():
     aqi_data = pd.read_csv('china_city_aqi.csv')
     # print(aqi_data.head(5))
     # print(aqi_data.min())
-    print('基本信息')
-    print(aqi_data.info())
+    # print('基本信息')
+    # print(aqi_data.info())
+
+    # 排名前十个城市
+    top_city_aqi = aqi_data.sort_values(by='AQI').head(10)
+    print('空气质量排名前十的城市：')
+    print(top_city_aqi)
+    print()
+
+    # 空气质量最差的十个城市
+    bottom_city_aqi = aqi_data.sort_values(by='AQI').tail(10)
+    print('空气质量最差的十个城市：')
+    print(bottom_city_aqi)
+
+    top_city_aqi.to_csv('top10.csv',index = False)
+
 if __name__ == '__main__':
     main()
