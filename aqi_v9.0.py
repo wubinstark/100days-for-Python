ad79e25e5033ff64fd37fcdacac319c46a863367
Logani <2322101957@qq.com>
2020-08-10 09:11:27 +0800

    0910

--- aqi_v9.0.py
+++ aqi_v9.0.py
@@ -1,8 +1,11 @@
-from selenium import webdriver
-
+import requests
 
 def main():
-
+    response = requests.get('http://www.baidu.com')
+    print(response.status_code)
+    print(type(response))
+    # print(response.text)
+    print(response.cookies)
 
 if __name__ == '__main__':
     main()
