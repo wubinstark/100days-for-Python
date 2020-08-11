00c759ce43c16b0b228622dbd09abebb8319139f
Logani <2322101957@qq.com>
2020-08-11 22:47:35 +0800

    0811

--- aqi_v9.0.py
+++ aqi_v9.0.py
@@ -1,11 +1,12 @@
 import requests
 
 def main():
-    response = requests.get('http://www.baidu.com')
+    response = requests.get('http://old.ibos.ixuenong.com/')
     print(response.status_code)
     print(type(response))
-    # print(response.text)
-    print(response.cookies)
+    print(response.text)
+    # print(response.cookies)
+    # print(response.headers)
 
 if __name__ == '__main__':
     main()
